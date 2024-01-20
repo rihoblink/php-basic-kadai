a93a62e7152948e8335192cab3029793d5c80545
riho-abeno <152072440+rihoblink@users.noreply.github.com>
2024-01-21 05:25:27 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta cherset="utf-8">
+  <title>PHP基礎編課題7</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $parsonal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($parsonal_data);
+    ?>
+  </p>
+</body>
+</html>
