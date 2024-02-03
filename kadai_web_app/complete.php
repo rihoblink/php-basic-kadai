7f04bac0db61a1df9e7ca915734f6eaa5cd582a0
riho-abeno <152072440+rihoblink@users.noreply.github.com>
2024-02-04 03:39:21 +0900

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP基礎編</title>
+  </head>
+  <body>
+    <h2>登録が完了しました。</h2>
+    <p>社員情報がデータベースに保存されました。</p>
+    <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+  </body>
+</html>
